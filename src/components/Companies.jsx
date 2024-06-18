a95c72c60991bc5b75e6bf6b10041a48e1bcd51b
suraj <surajkumarbehera110@gmail.com>
2024-06-18 18:41:23 +0530

    responsiveness new section added

--- src/components/Companies.jsx
+++ src/components/Companies.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Companies = () => {
     return (
-        <div className='px-4 lg:px-14 max-w-screen-2xl mx-auto my-8 bg-[#ADEF8866] pb-6'>
+        <div className='px-4 lg:px-14 max-w-screen-2xl mx-auto my-8  pb-6'>
             <div className='text-center md:w-3/4 mx-auto flex flex-col items-center pt-8' data-aos="zoom-out" data-aos-duration="1000" data-aos-offset="170" data-aos-easing="ease-in-out">
                 <h2 className='text-4xl text-gray-500 font-semibold mb-4'>
                     Companies</h2>
@@ -32,7 +32,7 @@ const Companies = () => {
                         <img className='w-24' src='/src/assets/youtube.svg' alt="YouTube" />
                     </li>
                 </ul>
-                <ul class="flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-none animate-infinite-scroll" aria-hidden="true">
+                <ul class="flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-none animate-infinite-scroll invisible lg:visible" aria-hidden="true">
                     <li>
                         <img className='w-24' src='/src/assets/chart.svg' alt="Chart" />
                     </li>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,50 +6,52 @@ import Agri2 from '../assets/agri2.png'
 import Agri3 from '../assets/agri3.png'
 
 const Home = () => {
-  return (
-    <div className='bg-[#ADEF8866] ' id="home">
-        <div className='px-4 lg:px-14 max-w-screen-2xl mx-auto min-h-screen h-screen '>
-            <Carousel className=' mx-auto'>
-                <div className="mb-8 md:mb-28 md:pb-8 flex flex-col md:flex-row-reverse items-center justify-evenly lg:gap-12">
-                    <div >
-                        <img className="animate-bounceSlow max-w-sm md:max-w-md lg:max-w-[36rem] " src={Agri1} alt='skb' />
+    return (
+        <div className='bg-[#ADEF8866] ' id="home">
+            <div className='px-4 lg:px-14 max-w-screen-2xl mx-auto min-h-screen h-screen '>
+
+                <Carousel className=' mx-auto'>
+                    <div className="mb-8 md:mb-28 md:pb-8 flex flex-col md:flex-row-reverse items-center justify-evenly lg:gap-12">
+                        <div >
+                            <img className="animate-bounceSlow max-w-sm md:max-w-md lg:max-w-[36rem] " src={Agri1} alt='skb' />
+                        </div>
+                        <div className="md:w-1/2  md:visible">
+                            <h1 className="text-4xl md:text-5xl font-semibold mb-4 text-gray-500 md:w-3/4 leading-snug">Her Education make your
+                                <span className="text-green-500 leading-snug"> Family Smile</span></h1>
+                            <p className="text-gray-500 text-base mb-8">Support,Contribute,Donate for the mission</p>
+                            <button className="btn-primary">Register</button>
+                        </div>
                     </div>
-                    <div className="md:w-1/2  md:visible">
-                        <h1 className="text-4xl md:text-5xl font-semibold mb-4 text-gray-500 md:w-3/4 leading-snug">Her Education make your 
-                        <span className="text-green-500 leading-snug"> Family Smile</span></h1>
-                        <p className="text-gray-500 text-base mb-8">Support,Contribute,Donate for the mission</p>
-                        <button className="btn-primary">Register</button>
+                    <div className="mb-8 md:mb-28 md:pb-8 flex flex-col md:flex-row-reverse items-center justify-evenly lg:gap-12">
+                        <div>
+                            <img className="animate-bounceSlow max-w-sm md:max-w-md lg:max-w-[36rem] " src={Agri3} alt='skb' />
+                        </div>
+                        <div className="md:w-1/2 md:visible">
+                            <h1 className="text-4xl md:text-5xl font-semibold mb-4 text-gray-500 md:w-3/4 leading-snug">Her Education make your
+                                <span className="text-green-500 leading-snug"> Family Smile</span></h1>
+                            <p className="text-gray-500 text-base mb-8">Support,Contribute,Donate for the mission</p>
+                            <button className="btn-primary">
+                                Register</button>
+                        </div>
                     </div>
-                </div>
-                <div className="mb-8 md:mb-28 md:pb-8 flex flex-col md:flex-row-reverse items-center justify-evenly lg:gap-12">
-                    <div>
-                        <img className="animate-bounceSlow max-w-sm md:max-w-md lg:max-w-[36rem] " src={Agri3} alt='skb' />
+                    <div className="mb-8 md:mb-28 md:pb-8 flex flex-col md:flex-row-reverse items-center justify-evenly lg:gap-12">
+                        <div>
+                            <img className="animate-bounceSlow max-w-[20rem] md:max-w-[25] lg:max-w-[29rem] " src={Agri2} alt='skb' />
+                        </div>
+                        <div className="md:w-1/2 md:visible">
+                            <h1 className="text-4xl md:text-5xl font-semibold mb-4 text-gray-500 md:w-3/4 leading-snug">Her Education make your
+                                <span className="text-green-500 leading-snug"> Family Smile</span></h1>
+                            <p className="text-gray-500 text-base mb-8">Support,Contribute,Donate for the mission</p>
+                            <button className="btn-primary">
+                                Register</button>
+                        </div>
                     </div>
-                    <div className="md:w-1/2 md:visible">
-                        <h1 className="text-4xl md:text-5xl font-semibold mb-4 text-gray-500 md:w-3/4 leading-snug">Her Education make your 
-                        <span className="text-green-500 leading-snug"> Family Smile</span></h1>
-                        <p className="text-gray-500 text-base mb-8">Support,Contribute,Donate for the mission</p>
-                        <button className="btn-primary">
-                            Register</button>
-                    </div>
-                </div>
-                <div className="mb-8 md:mb-28 md:pb-8 flex flex-col md:flex-row-reverse items-center justify-evenly lg:gap-12">
-                    <div>
-                        <img className="animate-bounceSlow max-w-[20rem] md:max-w-[25] lg:max-w-[29rem] " src={Agri2} alt='skb' />
-                    </div>
-                    <div className="md:w-1/2 md:visible">
-                        <h1 className="text-4xl md:text-5xl font-semibold mb-4 text-gray-500 md:w-3/4 leading-snug">Her Education make your 
-                        <span className="text-green-500 leading-snug"> Family Smile</span></h1>
-                        <p className="text-gray-500 text-base mb-8">Support,Contribute,Donate for the mission</p>
-                        <button className="btn-primary">
-                            Register</button>
-                    </div>
-                </div>
-                
-             </Carousel>
+
+                </Carousel>
+
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Home
